5839b56d775efb0ac289daab389c40a1520abdf6
rookiecol <juan.diegam@hotmail.com>
2023-08-22 01:09:45 -0500

    :boom: Beta 1.1.0
    scrapper listing values over the console and CO2EventConsumption schema

--- co2myclimate.py
+++ co2myclimate.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.chrome.service import Service as ChromeService
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
-
 
 service = ChromeService()
 driver = webdriver.Chrome(service=service)
@@ -21,6 +21,7 @@ def is_button_clickable():
         return element.is_displayed() and element.is_enabled()
     except:
         return False
+
 
 # Keep looping until the button is clickable
 while not is_button_clickable():
@@ -81,7 +82,6 @@ air_conditioned_area = driver.find_element(
 air_conditioned_area.send_keys('500')
 
 
-
 # Construct the JavaScript code to find the button element
 button_js = "return document.querySelector('div[class=\"carousel-item active\"] a[role=\"button\"]')"
 
@@ -93,157 +93,209 @@ button.click()
 
 # Finding the input element for number of people arriving by car by ID
 people_arriving_by_car = driver.find_element(
-    By.ID,'event_calculator_car_number_of_visitors')
+    By.ID, 'event_calculator_car_number_of_visitors')
 
 # Sending keys to the input field to set the number of people arriving by car
 people_arriving_by_car.send_keys('51900')
 
 
-
-average_distance_traveled_car = driver.find_element(By.ID,'event_calculator_car_average_distance_km')
+average_distance_traveled_car = driver.find_element(
+    By.ID, 'event_calculator_car_average_distance_km')
 average_distance_traveled_car.send_keys('30')
 
 
 # Finding the input element for number of people traveling by public transport by ID
 people_traveling_by_public_trasport = driver.find_element(
-    By.ID,'event_calculator_bus_or_train_number_of_visitors')
+    By.ID, 'event_calculator_bus_or_train_number_of_visitors')
 
 # Sending keys to the input field to set the number of people traveling by public transport
 people_traveling_by_public_trasport.send_keys('80000')
 
 # Finding the input element for average distance traveled by car by ID
 average_distance_traveled_public = driver.find_element(
-    By.ID,'event_calculator_bus_or_train_average_distance_km')
+    By.ID, 'event_calculator_bus_or_train_average_distance_km')
 
 # Sending keys to the input field to set the average distance traveled by car
 average_distance_traveled_public.send_keys('40')
 
 # Finding the input element for short-haul flights by ID
-short_hault_flights = driver.find_element(By.ID,'event_calculator_short_flights_amount')
+short_hault_flights = driver.find_element(
+    By.ID, 'event_calculator_short_flights_amount')
 
 # Sending keys to the input field to set the number of short-haul flights
 short_hault_flights.send_keys('500')
 
-medium_haul_flights = driver.find_element(By.ID,'event_calculator_medium_flights_amount')
+medium_haul_flights = driver.find_element(
+    By.ID, 'event_calculator_medium_flights_amount')
 
 medium_haul_flights.send_keys('30')
 
-long_flights_amount_input = driver.find_element(By.ID, 'event_calculator_long_flights_amount')
+long_flights_amount_input = driver.find_element(
+    By.ID, 'event_calculator_long_flights_amount')
 long_flights_amount_input.send_keys('5')
 
 
-percentage_bussines_class = driver.find_element(By.ID,'event_calculator_business_or_first_flights_per_cent')
+percentage_bussines_class = driver.find_element(
+    By.ID, 'event_calculator_business_or_first_flights_per_cent')
 percentage_bussines_class.send_keys('5')
 
-button_2 = driver.find_element(By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
+button_2 = driver.find_element(
+    By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
 button_2.click()
 
 
-
-input_2_3_star = driver.find_element(By.ID, 'event_calculator_stay_number_3_stars')
-input_2_3_star.send_keys('10')  # Replace '10' with your desired value
+over_night_stay_three_stars = driver.find_element(
+    By.ID, 'event_calculator_stay_number_3_stars')
+# Replace '10' with your desired value
+over_night_stay_three_stars.send_keys('10')
 
 # Similar code for the other input fields (4-star and 5-star hotels)
-input_4_star = driver.find_element(By.ID, 'event_calculator_stay_number_4_stars')
-input_4_star.send_keys('5')  # Replace '5' with your desired value
+over_night_stay_four_stars = driver.find_element(
+    By.ID, 'event_calculator_stay_number_4_stars')
+# Replace '5' with your desired value
+over_night_stay_four_stars.send_keys('5')
 
-input_5_star = driver.find_element(By.ID, 'event_calculator_stay_number_5_stars')
-input_5_star.send_keys('3')  # Replace '3' with your desired value
+over_night_stay_five_stars = driver.find_element(
+    By.ID, 'event_calculator_stay_number_5_stars')
+# Replace '3' with your desired value
+over_night_stay_five_stars.send_keys('3')
 
 
-button_3 = driver.find_element(By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
+button_3 = driver.find_element(
+    By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
 button_3.click()
 
 
-warm_meal_meat_amount_input = driver.find_element(By.ID, 'event_calculator_warm_meal_meat_amount')
-warm_meal_meat_amount_input.send_keys('50')  # Replace '50' with your desired value
+meal_meat_amount = driver.find_element(
+    By.ID, 'event_calculator_warm_meal_meat_amount')
+meal_meat_amount.send_keys('50')  # Replace '50' with your desired value
 
-warm_meal_vegetarian_amount_input = driver.find_element(By.ID, 'event_calculator_warm_meal_vegetarian_amount')
-warm_meal_vegetarian_amount_input.send_keys('30')  # Replace '30' with your desired value
+meal_vegetarian_amount = driver.find_element(
+    By.ID, 'event_calculator_warm_meal_vegetarian_amount')
+meal_vegetarian_amount.send_keys('30')  # Replace '30' with your desired value
 
-snacks_amount_input = driver.find_element(By.ID, 'event_calculator_snacks')
-snacks_amount_input.send_keys('100')  # Replace '100' with your desired value
+snacks_amount = driver.find_element(By.ID, 'event_calculator_snacks')
+snacks_amount.send_keys('100')  # Replace '100' with your desired value
 
-soda_liters_input = driver.find_element(By.ID, 'event_calculator_soda_liters')
-soda_liters_input.send_keys('20')  # Replace '20' with your desired value
+soda_liters = driver.find_element(By.ID, 'event_calculator_soda_liters')
+soda_liters.send_keys('200')  # Replace '20' with your desired value
 
-coffee_portions_input = driver.find_element(By.ID, 'event_calculator_coffee_portions')
-coffee_portions_input.send_keys('100')  # Replace '100' with your desired value
+coffee_cups = driver.find_element(By.ID, 'event_calculator_coffee_portions')
+coffee_cups.send_keys('100')  # Replace '100' with your desired value
 
-tea_portions_input = driver.find_element(By.ID, 'event_calculator_tea_portions')
-tea_portions_input.send_keys('80')  # Replace '80' with your desired value
+tea_cups = driver.find_element(By.ID, 'event_calculator_tea_portions')
+tea_cups.send_keys('80')  # Replace '80' with your desired value
 
-wine_liters_input = driver.find_element(By.ID, 'event_calculator_wine_liters')
-wine_liters_input.send_keys('15.5')  # Replace '15.5' with your desired value
+wine_liters = driver.find_element(By.ID, 'event_calculator_wine_liters')
+wine_liters.send_keys('15.5')  # Replace '15.5' with your desired value
 
-beer_liters_input = driver.find_element(By.ID, 'event_calculator_beer_liters')
-beer_liters_input.send_keys('30')  # Replace '30' with your desired value
+beer_liters = driver.find_element(By.ID, 'event_calculator_beer_liters')
+beer_liters.send_keys('30')  # Replace '30' with your desired value
 
 # Spirits Liters
-spirits_liters_input = driver.find_element(By.ID, 'event_calculator_spirits_liters')
-spirits_liters_input.send_keys('10.2')  # Replace '10.2' with your desired value
+spirits_liters = driver.find_element(
+    By.ID, 'event_calculator_spirits_liters')
+# Replace '10.2' with your desired value
+spirits_liters.send_keys('10.2')
 
-button_4 = driver.find_element(By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
+button_4 = driver.find_element(
+    By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
 button_4.click()
 
 
 # Power Consumption
-power_consumption_input = driver.find_element(By.ID, 'event_calculator_power_consumption_kwh')
-power_consumption_input.send_keys('150')  # Replace '150' with your desired value
+power_consumption = driver.find_element(
+    By.ID, 'event_calculator_power_consumption_kwh')
+# Replace '150' with your desired value
+power_consumption.send_keys('1500')
 
-button_5 = driver.find_element(By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
+button_5 = driver.find_element(
+    By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
 button_5.click()
 # Printed Matter
-printed_matters_input = driver.find_element(By.ID, 'event_calculator_printed_matters_kg')
-printed_matters_input.send_keys('10.5')  # Replace with the desired value
+printed_matters = driver.find_element(
+    By.ID, 'event_calculator_printed_matters_kg')
+printed_matters.send_keys('10.5')  # Replace with the desired value
 
 # Synthetic Materials
-synthetic_materials_input = driver.find_element(By.ID, 'event_calculator_synthetic_materials')
-synthetic_materials_input.send_keys('7.2')  # Replace with the desired value
+plastics = driver.find_element(
+    By.ID, 'event_calculator_synthetic_materials')
+plastics.send_keys('200')  # Replace with the desired value
 
 # Recyclable Products
-recyclable_products_input = driver.find_element(By.ID, 'event_calculator_recyclable_products')
-recyclable_products_input.send_keys('5.0')  # Replace with the desired value
+recyclable_material = driver.find_element(
+    By.ID, 'event_calculator_recyclable_products')
+recyclable_material.send_keys('100')  # Replace with the desired value
 
 # Vegetable Products
-vegetable_products_input = driver.find_element(By.ID, 'event_calculator_vegetable_products')
-vegetable_products_input.send_keys('3.5')  # Replace with the desired value
+plant_based_materials = driver.find_element(
+    By.ID, 'event_calculator_vegetable_products')
+plant_based_materials.send_keys('100')  # Replace with the desired value
 
 # Area of the Stand
-event_stand_area_input = driver.find_element(By.ID, 'event_calculator_event_stand_area')
-event_stand_area_input.send_keys('50.0')  # Replace with the desired value
+event_stand_area = driver.find_element(
+    By.ID, 'event_calculator_event_stand_area')
+event_stand_area.send_keys('50.0')  # Replace with the desired value
 
-button_6 = driver.find_element(By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
+button_6 = driver.find_element(
+    By.CSS_SELECTOR, 'div[class=\'carousel-item active\'] a:nth-child(2)')
 button_6.click()
 
 # Transported Weight
-transported_weight_input = driver.find_element(By.ID, 'event_calculator_transported_weight')
-transported_weight_input.send_keys('3.5')  # Replace '3.5' with your desired value
+transported_weight = driver.find_element(
+    By.ID, 'event_calculator_transported_weight')
+# Replace '3.5' with your desired value
+transported_weight.send_keys('12000')
 
 # Transported Distance
-transported_distance_input = driver.find_element(By.ID, 'event_calculator_transported_distance')
-transported_distance_input.send_keys('120')  # Replace '120' with your desired value
+transported_distance = driver.find_element(
+    By.ID, 'event_calculator_transported_distance')
+# Replace '120' with your desired value
+transported_distance.send_keys('120')
 
 # Residual Waste
-garbage_kg_input = driver.find_element(By.ID, 'event_calculator_garbage_kg')
-garbage_kg_input.send_keys('50')  # Replace '50' with your desired value
+garbage = driver.find_element(By.ID, 'event_calculator_garbage_kg')
+garbage.send_keys('1000')  # Replace '50' with your desired value
 
 # Recycling Waste
-garbage_recycling_kg_input = driver.find_element(By.ID, 'event_calculator_garbage_recycling_kg')
-garbage_recycling_kg_input.send_keys('20')  # Replace '20' with your desired value
+recycling = driver.find_element(
+    By.ID, 'event_calculator_garbage_recycling_kg')
+# Replace '20' with your desired value
+recycling.send_keys('200')
 
 # Find the form element with ID 'new_event_calculator'
 form_element = driver.find_element(By.ID, 'new_event_calculator')
-
-
-
 # Submit the form
 form_element.submit()
 
+wait = WebDriverWait(driver, 5)
+
+# Wait for the CO2 amount details to be displayed
+co2_amount_element = wait.until(
+    EC.presence_of_element_located((By.ID, 'co2_amount'))
+)
+
+# Extract the CO2 amount from the text (removing unnecessary characters and spaces)
+co2_amount_text = co2_amount_element.text
+co2_amount = co2_amount_text.replace(
+    'CO2 amount:', '').replace('t', '').replace(',', '').strip()
+
+# Log the CO2 amount to the console
+print(f"CO2 Amount: {co2_amount} t")
+
+# Locate and wait for the alert section to be displayed
+alert_section = wait.until(
+    EC.presence_of_element_located((By.CLASS_NAME, 'alert-gray'))
+)
+
+# Extract data from the alert section
+sections = alert_section.find_elements(By.CLASS_NAME, 'js-highlight-section')
+
+# Loop through the sections and extract data
+for section in sections:
+    section_name = section.find_element(By.TAG_NAME, 'dt').text
+    section_amount = section.find_element(By.TAG_NAME, 'dd').text
+    print(f"{section_name}: {section_amount}")
+
+# Pause execution for a while
 time.sleep(100)
-
-
-
-
-
-  
